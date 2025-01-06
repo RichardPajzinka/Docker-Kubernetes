73bb5b5b0e9601d515d2e543900a6a2e3d6bbf19
RichardPajzinka <rpajzinka@gmail.com>
2025-01-06 11:53:12 +0100

    final_version_web_visit_tracker
    final_version_web_visit_tracker

--- projects/web visit_tracker/index.js
+++ projects/web visit_tracker/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const redis = require('redis');
+const process = require('process')
 
 const app = express();
-const client = redis.createClient();
+const client = redis.createClient({
+    //host: 'https://my-redis-server.com'
+    host: 'redis-server',
+    //defualt 6379
+    port: 6379
+});
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
+    //process.exit(0);
     client.get('visits', (err,visits) => {
         res.send('Number of visits is ' + visits);
         client.set('visits', parseInt(visits) + 1);
@@ -13,5 +20,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(8081, () => {
-    console.log('Listening on port 8081');
+    console.log('Listening on port 4001');
 });
